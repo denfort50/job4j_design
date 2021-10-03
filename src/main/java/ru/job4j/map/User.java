221bb4c4f77835d662d151eefdfab8bb95e804f7
denfort50 <denfort50@yandex.ru>
2021-10-03 11:16:31 +0300

    Revert "2. Без переопределения equals и hashCode [#1005]"
    This reverts commit ba53c7409b55b054e9d488cefc0dff68186e3bee.

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,6 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
 
 public class User {
     private String name;
@@ -15,14 +12,4 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
-
-    public static void main(String[] args) {
-        User one = new User("Denis", 0, new GregorianCalendar(1995, Calendar.MARCH, 20));
-        User two = new User("Jack", 1, new GregorianCalendar(1972, Calendar.APRIL, 10));
-        Map<User, Object> map = new HashMap<>();
-        map.put(one, new Object());
-        map.put(two, new Object());
-        System.out.println(map);
-    }
-
 }
