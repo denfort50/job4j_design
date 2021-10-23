5aa677c649a9d2837b173334e84b8e97037485fc
denfort50 <denfort50@yandex.ru>
2021-10-23 18:15:52 +0300

    Revert "1. Создать элементарную структуру дерева [#1711]"
    This reverts commit 9192041a142e027f7cc1234b7398007a9c5624e5.

--- src/main/java/ru/job4j/tree/Tree.java
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -1,21 +1,2 @@
-package ru.job4j.tree;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-public interface Tree<E> {
-
-    boolean add(E parent, E child);
-
-    Optional<Node<E>> findBy(E value);
-
-    class Node<E> {
-        final E value;
-        final List<Node<E>> children = new ArrayList<>();
-
-        public Node(E value) {
-            this.value = value;
-        }
-    }
+package ru.job4j.tree;public interface Tree {
 }
