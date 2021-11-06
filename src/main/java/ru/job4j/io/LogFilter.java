0623482aa86591a3c113fc24d0c5e88ecbc89f36
denfort50 <denfort50@yandex.ru>
2021-11-06 22:51:07 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> result = new ArrayList<>();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(file));
+            List<String> strings = in.lines().toList();
+            result = strings.stream().filter(s -> {
+                String[] values = s.split(" ");
+                int penultimate = Integer.parseInt(values[values.length - 2]);
+                return penultimate == 404;
+            }).collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        log.forEach(System.out::println);
+    }
+}
