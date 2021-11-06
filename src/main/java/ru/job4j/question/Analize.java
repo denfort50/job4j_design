f33f3aaaba5f7a10ba1a1ee111675325418f0fa5
denfort50 <denfort50@yandex.ru>
2021-11-06 15:28:58 +0300

    2. Статистика по коллекции. [#45889 #223213]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -7,27 +7,22 @@ import java.util.stream.Collectors;
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        Map<Integer, String> mapOfPrevious = previous.stream()
-                .collect(Collectors.toMap(User::getId, User::getName));
-        Map<Integer, String> mapOfCurrent = current.stream()
-                .collect(Collectors.toMap(User::getId, User::getName));
-        int changed = (int) mapOfCurrent.keySet()
-                .stream()
-                .filter(id -> mapOfPrevious.containsKey(id) && !mapOfPrevious.containsValue(mapOfCurrent.get(id)))
-                .count();
-        int added = (int) current.stream()
-                .filter(uCurr -> !previous.stream()
-                        .map(User::getId)
-                        .collect(Collectors.toSet())
-                        .contains(uCurr.getId()))
-                .count();
-        int deleted = (int) previous.stream()
-                .filter(uPrev -> !current.stream()
-                        .map(User::getId)
-                        .collect(Collectors.toSet())
-                        .contains(uPrev.getId()))
-                .count();
+        Map<Integer, User> map = current.stream()
+                .collect(Collectors.toMap(User::getId, user -> user));
+        int changed = 0;
+        int added = 0;
+        int deleted = 0;
+        int length = map.size();
+        for (User u : previous) {
+            User userMap = map.get(u.getId());
+            map.put(u.getId(), u);
+            if (userMap == null) {
+                deleted++;
+            } else if (!userMap.getName().equals(map.get(u.getId()).getName())) {
+                changed++;
+            }
+            added = length - previous.size() + deleted;
+        }
         return new Info(added, changed, deleted);
     }
-
 }
