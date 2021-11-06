b0291497e41e6cbbd198200e211930d72906eda6
denfort50 <denfort50@yandex.ru>
2021-11-06 15:47:38 +0300

    2. Статистика по коллекции. [#45889 #223213]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,27 +1,28 @@
 package ru.job4j.question;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        Map<Integer, User> map = current.stream()
+        Map<Integer, User> mapOfCurrent = current.stream()
                 .collect(Collectors.toMap(User::getId, user -> user));
         int changed = 0;
         int added = 0;
         int deleted = 0;
-        int length = map.size();
-        for (User u : previous) {
-            User userMap = map.get(u.getId());
-            map.put(u.getId(), u);
-            if (userMap == null) {
+        int lengthOfMap = mapOfCurrent.size();
+        for (User userPrev : previous) {
+            User userCurr = mapOfCurrent.get(userPrev.getId());
+            mapOfCurrent.put(userPrev.getId(), userPrev);
+            if (userCurr == null) {
                 deleted++;
-            } else if (!userMap.getName().equals(map.get(u.getId()).getName())) {
+            } else if (!Objects.equals(userCurr.getName(), mapOfCurrent.get(userPrev.getId()).getName())) {
                 changed++;
             }
-            added = length - previous.size() + deleted;
+            added = lengthOfMap - previous.size() + deleted;
         }
         return new Info(added, changed, deleted);
     }
