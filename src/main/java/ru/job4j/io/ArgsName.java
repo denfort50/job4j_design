e08f07b5aa271a31c0013cfdaef5df2d769e5b29
denfort50 <denfort50@yandex.ru>
2021-11-18 08:29:37 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("String[] args is empty.");
+        }
+        Arrays.stream(args).forEach(s -> {
+            String[] parts = s.split("=");
+            if (parts.length == 1) {
+                throw new IllegalArgumentException("One of argument is incorrect.");
+            } else if (s.startsWith("-Xmx")) {
+                values.put("Xmx", parts[1]);
+            } else if (s.startsWith("-out")) {
+                values.put("out", parts[1]);
+            } else if (s.startsWith("-encoding")) {
+                values.put("encoding", parts[1]);
+            }
+        });
+    }
+
+        public static ArgsName of(String[]args) {
+            ArgsName names = new ArgsName();
+            names.parse(args);
+            return names;
+        }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
