55c9b2079bfa48999cd2f04b9aa9e4c035d9a066
denfort50 <denfort50@yandex.ru>
2021-11-24 08:19:36 +0300

    5.1. Именованные аргументы [#295518 #229870]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -16,26 +16,25 @@ public class ArgsName {
             throw new IllegalArgumentException("String[] args is empty.");
         }
         Arrays.stream(args).forEach(s -> {
-            String[] parts = s.split("=");
-            if (parts.length == 1) {
-                throw new IllegalArgumentException("One of argument is absent.");
-            } else {
-                values.put(parts[0].substring(1), parts[1]);
+            if (!s.startsWith("-") || s.startsWith("-=") || s.contains("==") || !s.contains("=") || s.endsWith("=")) {
+                throw new IllegalArgumentException("Arguments don't match the template.");
             }
+            String[] parts = s.split("=");
+            values.put(parts[0].substring(1), parts[1]);
         });
     }
 
-        public static ArgsName of(String[]args) {
-            ArgsName names = new ArgsName();
-            names.parse(args);
-            return names;
-        }
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
 
     public static void main(String[] args) {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
         System.out.println(jvm.get("Xmx"));
 
-        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
         System.out.println(zip.get("out"));
     }
 }
