52879acc67a0c1a6691692972e83bab082ab92f8
denfort50 <denfort50@yandex.ru>
2021-11-28 19:15:42 +0300

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,52 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public static void packFiles(List<File> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File file : sources) {
+                zip.putNextEntry(new ZipEntry(file.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Some arguments are absent.");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        File directory = new File(argsName.get("d"));
+        File exclude = new File(argsName.get("e"));
+        File target = new File(argsName.get("o"));
+        if (!directory.exists()) {
+            throw new IllegalArgumentException("The directory %s does not exist.");
+        }
+        List<File> sources = Search.search(directory.toPath(),
+                path -> !path.toFile().getName().endsWith(exclude.getName()))
+                .stream().map(Path::toFile).collect(Collectors.toList());
+        packFiles(sources, target);
+    }
+}
