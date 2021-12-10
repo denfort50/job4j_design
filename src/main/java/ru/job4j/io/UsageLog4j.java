9dc7cbf0a38ec172144f88544227fbf6616e358c
denfort50 <denfort50@yandex.ru>
2021-12-10 08:45:42 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Denis Kalchenko";
+        byte workExperience = 2;
+        short yearOfBorn = 1995;
+        int age = 26;
+        long motherlandPopulation = 145975300;
+        float eyesight = 1.0f;
+        double averageUniversityScore = 4.4;
+        char usingCurrency = '₽';
+        boolean married = true;
+        LOG.debug("User info name: {}, workExperience: {}, yearOfBorn: {}, age: {}, motherlandPopulation: {}, "
+                        + "eyesight: {}, averageUniversityScore: {},  usingCurrency: {}, married: {}",
+                name, workExperience, yearOfBorn, age, motherlandPopulation,
+                eyesight, averageUniversityScore, usingCurrency, married);
     }
 }
