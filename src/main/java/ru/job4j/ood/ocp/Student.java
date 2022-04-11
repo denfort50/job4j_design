3cf666d66567bce1337d324647368b1fa3ba8dd9
denfort50 <denfort50@yandex.ru>
2022-04-11 09:33:23 +0300

    0. Принцип открытости закрытости [#4914 #286906]

---
+++ src/main/java/ru/job4j/ood/ocp/Student.java
@@ -0,0 +1,27 @@
+package ru.job4j.ood.ocp;
+
+/**
+ * Класс описывает поведение студента
+ */
+public class Student {
+    private int age;
+    private int grade;
+
+    /**
+     * Метод реализует процесс обучения студента
+     * @param book – книга, которую изучает студент
+     * @return диссертацию, как результат учёбы
+     */
+    public String study(String book) {
+        return null;
+    }
+
+    /**
+     * Метод реализует процесс защиты диссертации
+     * @param thesis – защищаемая диссертация
+     * @return оценку студента комиссией
+     */
+    public double thesisDefend(String thesis) {
+        return 0.0;
+    }
+}
