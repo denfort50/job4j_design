d863bc64cb40774036368e184dfdb38734c78923
denfort50 <denfort50@yandex.ru>
2022-09-14 10:27:16 +0300

    2. Найти утечку памяти. [#504882]

---
+++ src/main/java/ru/job4j/gc/leak/CommentGenerator.java
@@ -0,0 +1,56 @@
+package ru.job4j.gc.leak;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CommentGenerator implements Generate {
+
+    public static final String PATH_PHRASES = "src/main/java/ru/job4j/gc/leak/files/phrases.txt";
+
+    public static final String SEPARATOR = System.lineSeparator();
+
+    private final List<Comment> comments = new ArrayList<>();
+
+    public static final Integer COUNT = 50;
+
+    private List<String> phrases;
+
+    private final UserGenerator userGenerator;
+
+    private final Random random;
+
+    public CommentGenerator(Random random, UserGenerator userGenerator) {
+        this.userGenerator = userGenerator;
+        this.random = random;
+        read();
+    }
+
+    private void read() {
+        try {
+            phrases = read(PATH_PHRASES);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    @Override
+    public void generate() {
+        comments.clear();
+        List<Integer> ints = new ArrayList<>();
+        random.ints(0, phrases.size())
+                .distinct().limit(3).forEach(ints::add);
+        for (int i = 0; i < COUNT; i++) {
+            String comment = phrases.get(ints.get(0)).concat(SEPARATOR)
+                    .concat(phrases.get(ints.get(1))).concat(SEPARATOR)
+                    .concat(phrases.get(ints.get(2)));
+            comments.add(new Comment(comment,
+                    userGenerator.randomUser()));
+        }
+    }
+}
